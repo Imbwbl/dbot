1ed99309bbe197289b39c710601f6be9e1b6859e
Nicolas Borboën <ponsfrilus@gmail.com>
2019-01-28 21:15:32 +0100

    Nouvelle commande `^^abo` qui donne le lien de la chaîne de T2006

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ client.on('ready', () => {
 
 // Surveille les messages
 client.on('message', msg => {
-  if (msg.content === 'ping') {  // Si le message dit "ping"
-    msg.reply('Pong!');          // Répond "Pong!"
+  if (msg.content === '^^ping') {     // Si le message dit "ping"
+    msg.reply('Pong! :ping_pong:');   // Répond "Pong!"
+  }
+  if (msg.content === '^^abo') {     // Si le message dit "ping"
+    msg.reply('Hey! Abonnez-vous à la châine de T2006 → https://www.youtube.com/channel/UCWC87vcR72VDYM7AGBzuBDQ :T2006:');   // Répond "Pong!"
   }
 });
 
